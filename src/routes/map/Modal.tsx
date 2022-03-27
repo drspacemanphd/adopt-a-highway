dccee2b324bfd225242c995ef5eb364633ce0f98
drspacemanphd <markabrown87@gmail.com>
2022-03-27 05:03:54 -0400

    filters

--- src/routes/map/Modal.tsx
+++ src/routes/map/Modal.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import { CheckboxField, Radio } from '@aws-amplify/ui-react';
+import { CheckboxField, Icon, Radio } from '@aws-amplify/ui-react';
+
 import { Filter } from './Filter';
 
-import './Legend.css';
+import './Modal.css';
 
-type LegendProps = {
+type ModalProps = {
+  open: boolean,
+  onClose: (e?: any) => any,
   filters: Array<Filter>,
   onChange: (e: {
     filterLabel: string,
@@ -14,7 +17,11 @@ type LegendProps = {
   }) => any
 }
 
-export function Legend(props: LegendProps) {
+export function Modal(props: ModalProps) {
+  if (!props.open) {
+    return null;
+  }
+
   const filters = props.filters || [];
 
   const sections = filters.map((filter: Record<string, any>) => {
@@ -59,7 +66,7 @@ export function Legend(props: LegendProps) {
             className='filter-option-radio'
             value={option.value}
             checked={option.selected}
-            onChange={(e) => props.onChange({
+            onChange={(_e: any) => props.onChange({
               filterLabel,
               filterName,
               optionLabel: option.label,
@@ -88,9 +95,21 @@ export function Legend(props: LegendProps) {
   });
 
   return (
-    <div className='ada-map-legend'>
-      {sections}
+    <div className='ada-map-modal'>
+      <div className='ada-map-modal-content'>
+        <div className='ada-map-modal-header'>
+          <Icon
+            ariaLabel='close-filter-modal'
+            pathData='M12 2C6.47 2 2 6.47 2 12C2 17.53 6.47 22 12 22C17.53 22 22 17.53 22 12C22 6.47 17.53 2 12 2ZM12 20C7.59 20 4 16.41 4 12C4 7.59 7.59 4 12 4C16.41 4 20 7.59 20 12C20 16.41 16.41 20 12 20ZM15.59 7L12 10.59L8.41 7L7 8.41L10.59 12L7 15.59L8.41 17L12 13.41L15.59 17L17 15.59L13.41 12L17 8.41L15.59 7Z'
+            viewBox={{ minX: 0, minY: 0, height: 16, width: 16 }}
+            onClick={(e: any) => props.onClose(e)}
+          />
+        </div>
+        <div className='ada-map-modal-main'>
+          {sections}
+        </div>
+        <div className='ada-map-modal-footer'></div>
+      </div>
     </div>
   );
 }
-
