509d0b38dd4dfe4a1d89c169ddb2fff6da78bc18
drspacemanphd <markabrown87@gmail.com>
2022-03-27 09:44:15 -0400

    remove arcgis core and replaced with loader

--- src/routes/map/Map.tsx
+++ src/routes/map/Map.tsx
@@ -1,74 +1,73 @@
-import { useEffect, useRef, useState } from 'react';
-import ArcGISMap from '@arcgis/core/Map';
-import MapView from '@arcgis/core/views/MapView';
-import FeatureLayer from '@arcgis/core/layers/FeatureLayer';
-import { Icon } from '@aws-amplify/ui-react';
+import { useEffect, useRef, useState } from "react";
 
-import { Legend } from './Legend';
-import { Modal } from './Modal';
-import { Filter } from './Filter';
-import { onSmallDevice } from '../../utils/on-small-device';
+import { loadModules } from "esri-loader";
+import { Icon } from "@aws-amplify/ui-react";
 
-import './Map.css';
+import { Legend } from "./Legend";
+import { Modal } from "./Modal";
+import { Filter } from "./Filter";
+import { onSmallDevice } from "../../utils/on-small-device";
+
+import "./Map.css";
 
 const defaultFilters: Record<string, Filter> = {
   Adoptable: {
-    label: 'Adopted',
-    filterName: 'Adoptable',
-    selectMode: 'multi',
+    label: "Adopted",
+    filterName: "Adoptable",
+    selectMode: "multi",
     options: [
       {
-        label: 'No',
-        value: 'N',
+        label: "No",
+        value: "N",
         selected: true,
       },
       {
-        label: 'Yes',
-        value: 'Y',
+        label: "Yes",
+        value: "Y",
         selected: true,
       },
     ],
   },
   CountyFull: {
-    label: 'County',
-    filterName: 'CountyFull',
-    selectMode: 'multi',
+    label: "County",
+    filterName: "CountyFull",
+    selectMode: "multi",
     options: [
       {
-        label: 'New Castle',
-        value: 'New Castle County',
+        label: "New Castle",
+        value: "New Castle County",
         selected: true,
       },
       {
-        label: 'Kent',
-        value: 'Kent County',
+        label: "Kent",
+        value: "Kent County",
         selected: false,
       },
       {
-        label: 'Sussex',
-        value: 'Sussex County',
+        label: "Sussex",
+        value: "Sussex County",
         selected: false,
       },
     ],
   },
   SUBMIT_DATE: {
-    label: 'Litter',
-    filterName: 'SUBMIT_DATE',
-    selectMode: 'single' as 'single' | 'multi',
+    label: "Litter",
+    filterName: "SUBMIT_DATE",
+    selectMode: "single" as "single" | "multi",
     options: [
       {
-        label: 'Past Week',
-        value: '7',
+        label: "Past Week",
+        value: "7",
         selected: true,
       },
       {
-        label: 'Past Month',
-        value: '30',
+        label: "Past Month",
+        value: "30",
         selected: false,
       },
       {
-        label: 'Past Year',
-        value: '365',
+        label: "Past Year",
+        value: "365",
         selected: false,
       },
     ],
@@ -87,7 +86,7 @@ const buildFilterExpression = (
         wrapper ? wrapper(option.value) : option.value
       }`;
     })
-    .join(' OR ');
+    .join(" OR ");
 };
 
 const updateFilters = (
@@ -105,7 +104,7 @@ const updateFilters = (
   filter.options.forEach((o: any) => {
     if (o.value === optionValue) {
       o.selected = !o.selected;
-    } else if (filter.selectMode === 'single') {
+    } else if (filter.selectMode === "single") {
       o.selected = false;
     }
   });
@@ -115,7 +114,11 @@ const updateFilters = (
 
 export function Map() {
   const mapContainerRef = useRef();
-  const arcGISMapRef = useRef({} as ArcGISMap);
+  const arcGISMapRef = useRef({} as __esri.Map);
+  const _Map = useRef();
+  const _MapView = useRef();
+  const _FeatureLayer = useRef();
+  const viewInstance = useRef({});
 
   const [filters, setFilters] = useState(defaultFilters);
   const [modalOpen, setModalOpen] = useState(false);
@@ -123,88 +126,127 @@ export function Map() {
   // Hooks
   useEffect(
     () => {
-      arcGISMapRef.current = new ArcGISMap({
-        basemap: 'gray-vector',
-      });
-
-      const view = new MapView({
-        map: arcGISMapRef.current,
-        container: mapContainerRef.current as any,
-        center: {
-          type: 'point',
-          latitude: 39.723555,
-          longitude: -75.658499,
-        },
-        constraints: {
-          geometry: {
-            type: 'extent',
-            xmin: -8436794.6354,
-            ymin: 4643205.1946,
-            xmax: -8354516.4673,
-            ymax: 4842698.0375,
-            spatialReference: {
-              wkid: 3857,
-            },
-          } as __esri.Extent,
-          minZoom: 10,
-        },
-        zoom: 10,
-      });
-
-      return () => {
-        view && view.destroy();
+      const loadMapModules = async () => {
+        return loadModules(["esri/Map", "esri/views/MapView"]).then(
+          ([ArcGISMap, MapView]) => {
+            _Map.current = ArcGISMap;
+            _MapView.current = MapView;
+            return loadMap();
+          }
+        );
       };
-    },
-    [] // On initial render
-  );
 
-  useEffect(() => {
-    const layers = arcGISMapRef.current.layers;
-    layers.removeAll();
+      const loadMap = async () => {
+        arcGISMapRef.current = new (_Map.current as any)({
+          basemap: "gray-vector",
+        });
 
-    const litterDefinitionExpression = buildFilterExpression(
-      filters['SUBMIT_DATE'],
-      '>',
-      (val: string) => `CURRENT_TIMESTAMP - INTERVAL '${val}' DAY`
-    );
-
-    arcGISMapRef.current.add(
-      new FeatureLayer({
-        url: process.env.REACT_APP_LITTER_FEATURE_SERVICE_LAYER_URL,
-        definitionExpression: litterDefinitionExpression || '1=1',
-        renderer: {
-          type: 'simple',
-          symbol: {
-            type: 'simple-marker',
-            color: 'rgba(200,90,0,1)',
-            size: 6,
-            outline: {
-              color: 'black',
-              width: '1px',
-            },
+        viewInstance.current = new (_MapView.current as any)({
+          map: arcGISMapRef.current,
+          container: mapContainerRef.current as any,
+          center: {
+            type: "point",
+            latitude: 39.723555,
+            longitude: -75.658499,
           },
-        } as __esri.RendererProperties,
-      })
-    );
+          constraints: {
+            geometry: {
+              type: "extent",
+              xmin: -8436794.6354,
+              ymin: 4643205.1946,
+              xmax: -8354516.4673,
+              ymax: 4842698.0375,
+              spatialReference: {
+                wkid: 3857,
+              },
+            },
+            minZoom: 10,
+          },
+          zoom: 10,
+        });
+      };
 
-    const roadsDefinitionExpression = ['Adoptable', 'CountyFull']
-      .map((filterName) => {
-        const filter = filters[filterName];
-        return `(${buildFilterExpression(
-          filter,
-          '=',
-          (val: string) => `'${val}'`
-        )})`;
-      })
-      .filter((expr: string) => expr !== '()');
+      const loadFeatureLayerModule = async () => {
+        return loadModules(["esri/layers/FeatureLayer"]).then(
+          ([FeatureLayer]) => {
+            _FeatureLayer.current = FeatureLayer;
+            renderFeatures();
+          }
+        );
+      };
 
-    arcGISMapRef.current.add(
-      new FeatureLayer({
-        url: process.env.REACT_APP_ROADS_FEATURE_SERVICE_LAYER_URL,
-        definitionExpression: roadsDefinitionExpression.join(' AND ') || '1=1',
-      })
-    );
-  }, [filters]);
+
+      const renderFeatures = async () => {
+        const layers = arcGISMapRef.current.layers;
+        layers.removeAll();
+
+        const litterDefinitionExpression = buildFilterExpression(
+          filters["SUBMIT_DATE"],
+          ">",
+          (val: string) => `CURRENT_TIMESTAMP - INTERVAL '${val}' DAY`
+        );
+
+        arcGISMapRef.current.add(
+          new (_FeatureLayer.current as any)({
+            url: process.env.REACT_APP_LITTER_FEATURE_SERVICE_LAYER_URL,
+            definitionExpression: litterDefinitionExpression || "1=1",
+            renderer: {
+              type: "simple",
+              symbol: {
+                type: "simple-marker",
+                color: "rgba(200,90,0,1)",
+                size: 6,
+                outline: {
+                  color: "black",
+                  width: "1px",
+                },
+              },
+            } as __esri.RendererProperties,
+          })
+        );
+
+        const roadsDefinitionExpression = ["Adoptable", "CountyFull"]
+          .map((filterName) => {
+            const filter = filters[filterName];
+            return `(${buildFilterExpression(
+              filter,
+              "=",
+              (val: string) => `'${val}'`
+            )})`;
+          })
+          .filter((expr: string) => expr !== "()");
+
+        arcGISMapRef.current.add(
+          new (_FeatureLayer.current as any)({
+            url: process.env.REACT_APP_ROADS_FEATURE_SERVICE_LAYER_URL,
+            definitionExpression:
+              roadsDefinitionExpression.join(" AND ") || "1=1",
+          })
+        );
+      };
+
+      // If on initial map load
+      if (!_Map.current || !_MapView.current || !_FeatureLayer.current) {
+        loadMapModules()
+          .then(() => {
+            return loadFeatureLayerModule();
+          })
+          .then(() => {
+            return () => {
+              viewInstance.current && (viewInstance.current as any).destroy();
+            };
+          });
+      } else {
+        renderFeatures()
+          .then(() => {
+            return () => {
+              viewInstance.current && (viewInstance.current as any).destroy();
+            };
+          });
+      }
+    },
+    [filters]
+  );
 
   // Render Helpers
   const renderModal = () => (
@@ -212,7 +254,7 @@ export function Map() {
       open={true}
       onClose={() => setModalOpen(false)}
       filters={Object.values(filters)}
-      onChange={(e) => {
+      onChange={(e: any) => {
         const updates = updateFilters(filters, e.filterName, e.optionValue);
         setFilters(updates);
       }}
@@ -221,34 +263,31 @@ export function Map() {
 
   const renderFilterIcon = () => (
     <Icon
-      className='ada-map-filters-mobile-icon'
-      role='button'
+      className="ada-map-filters-mobile-icon"
+      role="button"
       onClick={() => setModalOpen(true)}
-      ariaLabel='open-filter-modal'
-      pathData='M6.99999 6H17L11.99 12.3L6.99999 6ZM4.24999 5.61C6.26999 8.2 9.99999 13 9.99999 13V19C9.99999 19.55 10.45 20 11 20H13C13.55 20 14 19.55 14 19V13C14 13 17.72 8.2 19.74 5.61C20.25 4.95 19.78 4 18.95 4H5.03999C4.20999 4 3.73999 4.95 4.24999 5.61Z'
+      ariaLabel="open-filter-modal"
+      pathData="M6.99999 6H17L11.99 12.3L6.99999 6ZM4.24999 5.61C6.26999 8.2 9.99999 13 9.99999 13V19C9.99999 19.55 10.45 20 11 20H13C13.55 20 14 19.55 14 19V13C14 13 17.72 8.2 19.74 5.61C20.25 4.95 19.78 4 18.95 4H5.03999C4.20999 4 3.73999 4.95 4.24999 5.61Z"
       viewBox={{ minX: 0, minY: 0, height: 24, width: 24 }}
     />
   );
 
   return (
-    <div className='route-layout'>
-      <div id='ada-map-container'>
-        <div className='ada-map' ref={mapContainerRef}></div>
+    <div className="route-layout">
+      <div id="ada-map-container">
+        <div className="ada-map" ref={mapContainerRef}></div>
       </div>
-      {onSmallDevice()
-        ? renderFilterIcon()
-        : (
-          <Legend
-            filters={
-             Object.values(filters)
-            }
-            onChange={(e) => {
-              const updates = updateFilters(filters, e.filterName, e.optionValue);
-              setFilters(updates);
-            }}
-          />
-        )
-      }
+      {onSmallDevice() ? (
+        renderFilterIcon()
+      ) : (
+        <Legend
+          filters={Object.values(filters)}
+          onChange={(e) => {
+            const updates = updateFilters(filters, e.filterName, e.optionValue);
+            setFilters(updates);
+          }}
+        />
+      )}
       {onSmallDevice() && modalOpen ? renderModal() : null}
     </div>
   );
